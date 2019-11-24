b10933b36008fa2c331208c21edd203afd993798
Brendan McHugh <brendanmchugh12@gmail.com>
2019-11-24 19:26:49 +0100

    redux thunk setup

---
+++ front-end/src/reducers/index.js
@@ -0,0 +1,40 @@
+import {FETCH_DATA_LOADING,FETCH_DATA_LOADING_SUCCESS,FETCH_DATA_LOADING_FAILURE} from '../constants';
+import { combineReducers } from 'redux';
+
+
+
+export const holidays = (state = [],action) => {
+    switch(action.type) {
+        case FETCH_DATA_LOADING_SUCCESS:
+            return action.data
+        default:
+            return state;
+    }
+}
+
+export const itemLoading = (state = false,action) => {
+    switch(action.type) {
+        case FETCH_DATA_LOADING:
+                return action.status
+        default:
+            return state;
+    }
+} 
+
+export const itemLoadingError = (state = "",action) => {
+    switch(action.type) {
+        case FETCH_DATA_LOADING_FAILURE:
+                return action.itemLoadingError
+        default:
+            return state;
+    }
+}
+
+export const rootReducer = combineReducers({
+    holidays,
+    itemLoading,
+    itemLoadingError
+});
+
+export default rootReducer;
+
