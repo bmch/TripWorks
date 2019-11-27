22ec291dbc9b600d2e9c9ed1f0ae97b7f976c0e3
codeamo <amiinbizzle@gmail.com>
2019-11-27 09:50:57 +0100

    feat/fetched hotel API

--- front-end/src/services/__mock__/ApiClient.js
+++ front-end/src/services/__mock__/ApiClient.js
@@ -1,17 +1,27 @@
 const BASE_URL = 'https://apidojo-booking-v1.p.rapidapi.com/locations/auto-complete?languagecode=en-us&text=';
 
+
 export default {
   getAllHotels: (city) => {
     return fetchRequest(city);
     
   },
   getCityIdByName: (city) => {
-    return fetchRequest('');
+    return fetchRequest(city);
   }
 }
 
+//TODO: add this to env file
+const options = {
+  headers: {
+    "x-rapidapi-host": "apidojo-booking-v1.p.rapidapi.com",
+	"x-rapidapi-key": "c1a4f446b9msh0c76ada8c167fdap1b92aejsn40d8b5e89c91"
+  }
+} 
+
 const fetchRequest = (url) => {
-  return fetch(`${BASE_URL}/${url}`)
+console.log("TCL: fetchRequest -> url", url) 
+  return fetch(`${BASE_URL}${url}`, options)
     .then((res) => (res.status <= 400 ? res : Promise.reject(res)))
     .then((res) => res.json())
     .catch((error) => {
