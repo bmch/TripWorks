114c7ce0826f908fdc5f2eeeaed642f744e530f5
Erin <erin.eliz.costello@gmail.com>
2019-11-29 13:49:39 +0100

    feat/weather forecast component and weather component that gives weather for single goDate

--- front-end/src/actions/weather.js
+++ front-end/src/actions/weather.js
@@ -9,23 +9,18 @@ export const fetchDataWeather = (city) => (dispatch, getState) => {
 
       if (weather.data[i].valid_date === goDate) {
 
-        // for (let j = i; j < weather.data.length; j++) {
-        //   if (weather.data[i].valid_date !== backDate) {
-
-            dispatch({
-              type: 'SET_WEATHER',
-              data: {
-                city: weather.city_name,
-                icon: weather.data[i].weather.icon,
-                weather: weather.data[i].temp,
-                desc: weather.data[i].weather.description,
-                date: weather.data[i].valid_date,
-                forecast: weather.data
-              }
-            })
+        dispatch({
+          type: 'SET_WEATHER',
+          data: {
+            city: weather.city_name,
+            icon: weather.data[i].weather.icon,
+            weather: weather.data[i].temp,
+            desc: weather.data[i].weather.description,
+            date: weather.data[i].valid_date,
+            forecast: weather.data
           }
-      //   }
-      // }
+        })
+      }
     }
   })
 }
@@ -36,36 +31,22 @@ export const fetchDataWeatherTwo = (city) => (dispatch, getState) => {
     const { goDate, backDate } = getState().form.userInput.values
 
     for (let i = 0; i < weather.data.length; i++) {
-      // console.log(goDate);
 
       if (weather.data[i].valid_date === goDate) {
 
-        // for (let j = i; j < weather.data.length; j++) {
-        //   if (weather.data[j].valid_date !== backDate) {
-
-            dispatch({
-              type: 'SET_WEATHER_TWO',
-              data: {
-                city: weather.city_name,
-                icon: weather.data[i].weather.icon,
-                weather: weather.data[i].temp,
-                desc: weather.data[i].weather.description,
-                date: weather.data[i].valid_date,
-                forecast: weather.data
-              }
-            })
+        dispatch({
+          type: 'SET_WEATHER_TWO',
+          data: {
+            city: weather.city_name,
+            icon: weather.data[i].weather.icon,
+            weather: weather.data[i].temp,
+            desc: weather.data[i].weather.description,
+            date: weather.data[i].valid_date,
+            forecast: weather.data
           }
-      //   }
-      // }
+        })
+      }
     }
   })
 }
 
- // for (let j = i; j < weather.data.length; j++) {
-        //   if (weather.data[i].valid_date !== dateSelected.backDate) {
-
-        // const dateSelected = getState()
-    // for (let i = 0; i < weather.data.length; i++) {
-    //   if (weather.data[i].valid_date === dateSelected.goDate) {
-        // for (let j = i; j < weather.data.length; j++) {
-        //   if (weather.data[i].valid_date !== dateSelected.backDate) {
--- front-end/src/components/userInput/userInput.js
+++ front-end/src/components/userInput/userInput.js
@@ -37,10 +37,6 @@ const UserInput = ({ handleSubmit }) => {
     );
   };
 
-  // let go = formValues.goDate
-  // let back = formValues.backDate
-  // const dispatch = useDispatch();
-
   const onSubmit = formValues => {
     console.log(formValues);
     // dispatch(fetchHotelData(formValues));
@@ -52,11 +48,7 @@ const UserInput = ({ handleSubmit }) => {
   }
 
 
-  // this.props.history.push('/TripResults');
-// const wContComp = setTimeout(function(){ return <WCont /> }, 5000);
-    // console.log(formValues.goDate)
-
-    // console.log(formValues.goDate)
+ 
 
   
   return (
@@ -157,153 +149,3 @@ export default reduxForm({
   destroyOnUnmount: false
 })(UserInput);
 
-
-
-
-
-
-
-
-
-// import React, {useEffect} from 'react';
-// import {useDispatch, useSelector} from 'react-redux';
-// import {Field, reduxForm} from 'redux-form';
-      // import './userInput.css'
-// import {fetchDataWeather} from '../../actions/weather';
-// import {getWeather} from '/Users/erincostello/Desktop/Tripz/TripWorks/front-end/src/services/weather/weather-api.js';
-
-
-// const UserInput = ({handleSubmit}) => {
-
-
-
-//   // const getCity = () => {
-//   //   console.log('action dispatched');
-//   //   dispatch(fetchDataWeather(city))
-//   // }
-
-//   const renderError = ({ error, touched }) => {
-//     if (touched && error) {
-//       return (
-//       <div className="errorMsg">
-//         <div className="header">{error}</div>
-//       </div>
-//       );
-//     }
-//   }
-
-//   const renderInput = ({ input, meta }) => {
-//     console.log(meta);  
-//     const divError = ` field ${ meta.touched && meta.error ? 'error' : ''}`;
-//     // const typeChecker = `${meta.active && (input.name === 'goDate' || 'backDate') ? 'date' : 'text'}`
-//     return (
-//       <div className={divError}>
-//        <input { ...input } autoComplete="off" className="input" type="text"/>
-//         {renderError(meta)}
-//       </div>
-//     );
-//   }
-
-
-
-//   const onSubmit = (formValues) => {
-//   //   const city = useSelector(state => state.form.userInput.values.destination);
-//   // const dispatch = useDispatch();
-//   // dispatch(fetchDataWeather(city))
-
-//   //   console.log(formValues);  
-//   }
-
-//   // const fetchWeather = (city) => {
-//   //   getWeather(city)
-//   // }
-
-//   return (
-//     <div className="container">
-//       <form onSubmit={handleSubmit(onSubmit)} className="error">
-//         <div className="from">
-//           <div >
-//             <Field
-//               name="departure"
-//               component={renderInput}
-//               type="text"
-//               placeholder="From"
-//             />
-//           </div>
-//         </div>
-//         <div className="to">
-//           <div >
-//             <Field
-//               name="destination"
-//               component={renderInput}
-//               type="text"
-//               placeholder="To"
-//             />
-//           </div>
-//         </div>
-//           <div className="Wrapper">
-//             <div className="go">
-//               <div >
-//                 <Field
-//                   name="goDate"
-//                   component={renderInput}
-//                   type="date"
-//                   placeholder="go"
-//                   className="datepicker"
-//                 />
-//               </div>
-//           </div>
-//           <div className="back">
-//               <div >
-//                 <Field
-//                   name="backDate"
-//                   component={renderInput}
-//                   type="date"
-//                   placeholder="back"
-//                   className="datepicker"
-//                 />
-//               </div>
-//           </div>
-//           <div className="back">
-//               <div >
-//                 <Field
-//                   name="backDate"
-//                   component={renderInput}
-//                   type="date"
-//                   placeholder="back"
-//                   className="datepicker"
-//                 />
-//               </div>
-//           </div>
-//         </div>
-//         <div className="field">
-//           <div className="control">
-//             <button 
-//             className="input"
-//             // onClick={}
-//             >Submit</button>
-//           </div>
-//         </div>
-//       </form>
-//     </div>
-//   )
-// };
-
-// const inputValidator = values => {
-//   const errors = {};
-//   if (!values.departure) {
-//     errors.departure = 'departure city is required';
-//   } 
-//   if (!values.destination) {
-//     errors.destination = 'destination city is required';
-//   } 
-//   return errors;
-// };
-
-
-
-
-// export default reduxForm({
-//   form: 'userInput',
-//   validate: inputValidator
-// })(UserInput);
