6c1372e8d6ba32ba3a830f6c665eeaba0174ef5a
codeamo <amiinbizzle@gmail.com>
2019-11-29 15:46:38 +0100

    made small changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,20 @@ import React, { useEffect } from 'react';
 import UserInput from './components/userInput/userInput';
 import { useSelector } from "react-redux";
 import TripResults from "./containers/TripResults/index";
+import Result from "./components/result/result";
+import SignIn from './components/singIn/singIn';
+import PackageResult from './components/packageResult/packageResult'
+
 
 
 const App = () => {
 
     return (
-      <div>
-      <UserInput />
-      <TripResults />
+      <div className="App">
+        <UserInput />  
+        {/* <Result />  */}
+        {/* <SignIn/> */}
+        {/* <PackageResult /> */}
       </div>
     );
 };
