050bd77a3ca2280b31e2bd639d5e77d13e001ed4
Erin <erin.eliz.costello@gmail.com>
2019-12-02 12:30:14 +0100

    updating master

--- front-end/src/reducers/weather.js
+++ front-end/src/reducers/weather.js
@@ -1,6 +1,6 @@
 import { SET_WEATHER, SET_WEATHER_TWO } from '../constants';
 
-export const weather = (state = {}, action) => {
+export const weather = (state = [], action) => {
   switch (action.type) {
     case SET_WEATHER:
       return action.data
@@ -10,7 +10,7 @@ export const weather = (state = {}, action) => {
 
 }
 
-export const weatherTwo = (state = {}, action) => {
+export const weatherTwo = (state = [], action) => {
   switch (action.type) {
     case SET_WEATHER_TWO:
       return action.data
